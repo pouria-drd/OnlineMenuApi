ca00850862e05c220f53d986dbd7d076920b6e2a
Pour1a-D <pour1a.dra80@gmail.com>
2024-04-28 18:25:36 +0330

    product model and admin page done

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from products.models import Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    ordering = ["-created_at"]
+
+    list_display = [
+        "name",
+        "slug",
+        "category",
+        "is_active",
+        "created_at",
+        "updated_at",
+    ]
+
+    list_filter = ["category", "is_active", "created_at", "updated_at"]
+
+    search_fields = ["name", "slug", "category"]
+
+    prepopulated_fields = {"slug": ("name",)}
+
+    readonly_fields = ["created_at", "updated_at"]
+
+    autocomplete_fields = [
+        "category"
+    ]  # Allows selecting category from a dropdown instead of a text field
+
+    fieldsets = (
+        (None, {"fields": ("category", "name", "description", "slug")}),
+        ("Status", {"fields": ("is_active",)}),
+        (
+            "Timestamps",
+            {
+                "fields": ("created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    actions = ["activate_products", "deactivate_products"]
+
+    def activate_products(self, request, queryset):
+        queryset.update(is_active=True)
+
+    activate_products.short_description = "Activate selected products"
+
+    def deactivate_products(self, request, queryset):
+        queryset.update(is_active=False)
+
+    deactivate_products.short_description = "Deactivate selected products"
--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from categories.models import Category
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.RESTRICT
+    )
+    slug = models.SlugField(unique=True, blank=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
