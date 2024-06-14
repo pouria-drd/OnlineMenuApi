6afc0fd796850a1a9280541906b31a8e3e49cb68
Pour1a-D <pour1a.dra80@gmail.com>
2024-06-14 14:30:45 +0330

    retrun 404 error if there is no active menu

--- customer_panel/views.py
+++ customer_panel/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
 
@@ -10,6 +12,22 @@ class CustomerMenuCategoryListView(ListAPIView):
     permission_classes = [AllowAny]  # Allow access to anyone (public view)
     serializer_class = CustomerMenuCategorySerializer
 
+    def list(self, request, *args, **kwargs):
+        # Retrieve queryset of categories
+        queryset = self.get_queryset()
+
+        if queryset.exists():
+            # Serialize and return categories if queryset is not empty
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
+
+        else:
+            # Return custom response when no categories are found
+            return Response(
+                {"message": "No categories found for this menu."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
     def get_queryset(self):
         """
         Retrieve categories belonging to an active menu specified by 'menu_slug'.
@@ -19,8 +37,11 @@ class CustomerMenuCategoryListView(ListAPIView):
         menu_slug = self.kwargs.get("menu_slug")
 
         try:
+            # Attempt to retrieve the menu with the given slug
             menu = Menu.objects.get(slug=menu_slug, is_active=True)
+            # Retrieve categories related to the active menu
             return Category.objects.filter(menu=menu, is_active=True)
 
         except Menu.DoesNotExist:
+            # If the menu with the specified slug does not exist
             return Category.objects.none()
